8635bd35e67cf15325e4413438041bc29883d158
Michael Holroyd <meekohi@gmail.com>
2012-10-27 11:03:13 -0400

    javascript

---
+++ donorbadge.js
@@ -0,0 +1,44 @@
+// LETS DO THIS
+function setCookie(c_name,value,exdays)
+{
+	var exdate=new Date();
+	exdate.setDate(exdate.getDate() + exdays);
+	var c_value=escape(value) + ((exdays==null) ? "" : "; expires="+exdate.toUTCString());
+	document.cookie=c_name + "=" + c_value;
+}
+function getCookie(c_name)
+{
+	var i,x,y,ARRcookies=document.cookie.split(";");
+	for (i=0;i<ARRcookies.length;i++)
+	{
+		x=ARRcookies[i].substr(0,ARRcookies[i].indexOf("="));
+		y=ARRcookies[i].substr(ARRcookies[i].indexOf("=")+1);
+		x=x.replace(/^\s+|\s+$/g,"");
+		if (x==c_name)
+		{
+			return unescape(y);
+		}
+	}
+}
+function getParameterByName(name)
+{
+	name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
+	var regexS = "[\\?&]" + name + "=([^&#]*)";
+	var regex = new RegExp(regexS);
+	var results = regex.exec(window.location.search);
+	if(results == null)
+		return "";
+	else
+		return decodeURIComponent(results[1].replace(/\+/g, " "));
+}
+
+// This runs every time this file is loaded!
+// Checks the link for a donatorbadge_parent and moves it into a cookie.
+var parent = getCookie("donatorbadge_parent");
+if (parent==null && parent=="")
+{
+	console.log("DonatorBadge: no existing cookie.");
+	parent = getParameterByName("donatorbadge_parent");
+	setCookie("donatorbadge_parent",parent);
+}
+
