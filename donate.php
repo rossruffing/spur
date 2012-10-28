cd78e04fbe151780657b77451b7095a0943d266c
Krystal <krystalxxkat@gmail.com>
2012-10-27 22:04:10 -0400

    Add donations page

---
+++ donate.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<title>Thank You</title>
+	<link rel="stylesheet" href="style.css">
+	<script src="donorbadge.js"></script>
+</head>
+<body>
+	<div class="donate-body">
+		<b><div style="font-size:1.4em;color:#8195B5">What would you like to give to your kitten?</div></b>
+		<br />
+		<div class="donate-amnt">
+		<table cellpadding="5px" cellspacing="5px">
+				<tr><td><a href="?donate_amt=5">$5</a></td><td>will <b>save</b> a kitten from poachers</td></tr>
+				<tr><td><a href="?donate_amt=10">$10</a></td><td>will provide a kitten with <b>food and water</b></td></tr>
+				<tr><td><a href="?donate_amt=20">$20</a></td><td>will provide a kitten with <b>shelter</b></td></tr>
+				<tr><td><a href="?donate_amt=30">$30</a></td><td>will provide a kitten with a <b>grooming brush</b> and <b>litter bowl</b></td></tr>
+				<tr><td><a href="?donate_amt=50">$50</a></td><td>will cover fees for a kitten's <b>adoption</b></td></tr>
+				<tr><td><a href="?donate_amt=75">$75</a></td><td>will cover <b>medical expenses</b> for sick kittens</td></tr>
+				<tr><td><a href="?donate_amt=100">$100</a></td><td>will send a kitten to <b>school</b></td></tr>
+	</table>
+</div>
+</div>
+</body>
+</html>
